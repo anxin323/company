ab36a60371e0ce66e7b20f6b7e093ed0c92839ec
anxin323 <276284755@qq.com>
2015-07-16 16:48:34 +0800

    Spring+SpringMVC+Hibernate

---
+++ company Maven Webapp/src/main/java/com/alex/console/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.alex.console.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alex.base.controller.BaseController;
+import com.alex.base.vo.BaseResultVo;
+import com.alex.console.model.User;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+@Controller
+@RequestMapping(value = "restapi/user/")
+public class UserController extends BaseController{
+	@Autowired
+//	private IUserService userService;
+	
+	private static final Log LOGGER = LogFactory.getLog(UserController.class);
+
+	@RequestMapping(value = "addUser", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	@ApiOperation(value = "新增用户信息", httpMethod = "GET", notes = "填写完整的用户信息", response = BaseResultVo.class)
+	public @ResponseBody String addUser( @ApiParam(required = true) @ModelAttribute User user) {
+		user.setRigisterTime(System.currentTimeMillis());
+		return buildSuccessResultInfo(user);
+//		if (userService.saveUser(user)){
+//			return buildSuccessResultInfo(user);
+//		}else{
+//			return buildFailedResultInfo(200, "用户名已存在");
+//		}
+	}
+
+	@RequestMapping(value = "getUser",  produces = "application/json; charset=utf-8")
+	@ApiOperation(value = "获取用户信息", httpMethod = "POST", notes = "获取用户信息", response = BaseResultVo.class)
+	public @ResponseBody String getUser() {
+//		List<User> userList= userService.getUsers();
+		return buildSuccessResultInfo("");
+	}
+	
+	@RequestMapping(value = "verifyUser", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	@ApiOperation(value = "验证用户信息", httpMethod = "GET", notes = "请输入用户名和密码", response = BaseResultVo.class)
+	public @ResponseBody String verifyUser(@ApiParam(required = true) @RequestParam String userName,@ApiParam(required = true) @RequestParam String password) {
+		User user = new User();
+		user.setUsername(userName);
+		user.setPassword(password);
+//		if (userService.verifyUser(user)){
+//			return buildSuccessResultInfo(user);
+//		}else{
+//			return buildFailedResultInfo(200, "用户名或密码不正确");
+//		}
+		return buildSuccessResultInfo(user);
+	}
+}
---
+++ company Maven Webapp/src/main/java/com/alex/console/model/User.java
@@ -0,0 +1,98 @@
+package com.alex.console.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.joda.time.DateTime;
+
+//@Entity
+//@Table(name = "t_user")  
+@Entity(name="t_ichat_user")
+public class User {
+    // 主键自动增长
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+	private String password;
+    private String nickName;
+    private String mobile;
+    private String email;
+    private long loginTime;
+    private long rigisterTime;
+    private int status;
+    private int userType;
+    private String memo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getLoginTime() {
+		return loginTime;
+	}
+	public void setLoginTime(long loginTime) {
+		this.loginTime = loginTime;
+	}
+	public long getRigisterTime() {
+		return rigisterTime;
+	}
+	public void setRigisterTime(long rigisterTime) {
+		this.rigisterTime = rigisterTime;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getUserType() {
+		return userType;
+	}
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+    
+    
+	
+}
+
