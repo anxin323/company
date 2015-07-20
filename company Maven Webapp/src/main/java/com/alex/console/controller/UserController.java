8c2924cbfb6d45e919bfba2c11208ae84ec4868e
anxin323 <276284755@qq.com>
2015-07-20 17:11:36 +0800

    1、新增列表显示 2、新增登陆跳转

--- company Maven Webapp/src/main/java/com/alex/console/controller/UserController.java
+++ company Maven Webapp/src/main/java/com/alex/console/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.alex.console.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,11 +11,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.alex.base.controller.BaseController;
 import com.alex.base.vo.BaseResultVo;
@@ -49,14 +53,20 @@ public class UserController extends BaseController{
 
 	@RequestMapping(value = "background/getUser",  method = RequestMethod.GET,produces = "application/json; charset=utf-8")
 	@ApiOperation(value = "获取用户信息", httpMethod = "GET", notes = "获取用户信息", response = BaseResultVo.class)
-	public @ResponseBody String getUser() {
+	public @ResponseBody ModelAndView getUser(ModelMap model) {
 		List<User> userList= userService.getAllUser();
-		return buildSuccessResultInfo(userList);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("userList", userList);
+		mv.setViewName("userList");
+		return mv;
+//		model.addAttribute("userList", userList);
+//		return new ModelAndView("userList",model);
+		//return buildSuccessResultInfo(userList);
 	}
 	
 	@RequestMapping(value = "verifyUser", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
 	@ApiOperation(value = "验证用户信息", httpMethod = "POST", notes = "请输入用户名和密码", response = BaseResultVo.class)
-	public @ResponseBody String verifyUser(@ApiParam(required = true) @RequestParam String userName,@ApiParam(required = true) @RequestParam String password,HttpServletRequest request, HttpSession session) {
+	public String verifyUser(@ApiParam(required = true) @RequestParam String userName,@ApiParam(required = true) @RequestParam String password,HttpServletRequest request, HttpSession session) {
 		if("".equals(userName.trim())){
 			return buildFailedResultInfo(201, "用户名不能为空");
 		}else if("".equals(password.trim())){
@@ -69,9 +79,18 @@ public class UserController extends BaseController{
 		}
 		if(password.equals(user.getPassword())){
 			request.getSession().setAttribute("userInfo", user);
-			return buildSuccessResultInfo(user);
+			return "redirect:/console/user/background/getUser";
+			//return buildSuccessResultInfo(user);
 		}else{
 			return buildFailedResultInfo(201, "密码不正确");
 		}
 	}
+	
+	@RequestMapping("/demo2/show")  
+    public @ResponseBody Map<String, String> getMap() {  
+        Map<String, String> map = new HashMap<String, String>();  
+        map.put("key1", "value-1");  
+        map.put("key2", "value-2");  
+        return map;  
+    } 
 }
